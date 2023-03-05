02decdabe1612cb309913d200bc3b03374e8c9bc
Alejandro Castro <111087081+AlejandroC10P@users.noreply.github.com>
2023-03-04 19:12:01 -0500

    Ejercicio 4

---
+++ Ejercicio 4.py
@@ -0,0 +1,5 @@
+EdadJuan = int(input('Ingrese la edad de Juan: '))
+EdadAlberto = (2/3)*EdadJuan
+EdadAna = (4/3)*EdadJuan
+EdadMama = EdadJuan + EdadAlberto + EdadAna
+print(f'Las edades son:\nEdad Juan: {EdadJuan}\nEdad Alberto: {EdadAlberto}\nEdad Ana: {EdadAna}\nEdad Mamá: {EdadMama}')
